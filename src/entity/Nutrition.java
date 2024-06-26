4c008839d9a14182a9b837c633fad18dd217299f
seonghyunban <seonghyun.ban@pm.me>
2024-06-26 17:43:54 -0400

    Added Nutrition class to entity

--- src/entity/Nutrition.java
+++ src/entity/Nutrition.java
@@ -1,2 +1,17 @@
-package entity;public class Nutrition {
+package entity;
+
+public abstract class Nutrition {
+    private String label;
+    private String unit;
+    private float quantity;
+    private float percentage;
+
+    public Nutrition(String label, String unit, float quantity, float percentage) {
+        this.label = label;
+        this.unit = unit;
+        this.quantity = quantity;
+        this.percentage = percentage;
+    }
+
 }
+
