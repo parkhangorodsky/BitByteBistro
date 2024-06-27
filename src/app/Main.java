1d672c1ecfea5d418bd13d093ee6d78de05238c3
seonghyunban <seonghyun.ban@pm.me>
2024-06-26 21:00:47 -0400

    set searchRecipeView as the initial view of the application

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,15 +1,35 @@
 package app;
 
+import view.SearchRecipeView;
+
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        // Initialize Frame
         JFrame application = new JFrame();
         application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         application.setTitle("BitByteBistro");
         application.setLocationRelativeTo(null);
         application.setSize(800, 600);
+
+        // Initialize Layout and Panel
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        // Create SearchRecipeView
+        SearchRecipeView searchRecipeView = new SearchRecipeView();
+        views.add(searchRecipeView, searchRecipeView.viewname);
+
+        // Set current panel to searchRecipeView
+        CardLayout currPanel = (CardLayout) views.getLayout();
+        currPanel.show(views, searchRecipeView.viewname);
+
+        // Set application visible
         application.setVisible(true);
     }
 }
