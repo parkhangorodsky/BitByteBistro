642a7f81b523513b038764999ebcd88a13b97037
seonghyunban <seonghyun.ban@pm.me>
2024-06-27 02:13:49 -0400

    Implemented SearchRecipeInteractor

--- src/main/java/use_case/interactor/SearchRecipeInteractor.java
+++ src/main/java/use_case/interactor/SearchRecipeInteractor.java
@@ -1,18 +1,34 @@
 package use_case.interactor;
 
 import api.RecipeAPI;
+import entity.Recipe;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import use_case.input_data.InputBoundary;
 import use_case.input_data.SearchRecipeInputData;
 
-public class RecipeSearchInteractor implements InputBoundary {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchRecipeInteractor implements InputBoundary {
     private RecipeAPI recipeAPI;
 
-    public RecipeSearchInteractor(RecipeAPI recipeAPI) {
+    public SearchRecipeInteractor(RecipeAPI recipeAPI) {
         this.recipeAPI = recipeAPI;
     }
     @Override
     public void execute(SearchRecipeInputData searchRecipeInputData) {
         String queryString = searchRecipeInputData.getQueryString();
+        JSONArray recipesJSONArray = recipeAPI.getRecipe(queryString);
 
+        List<Recipe> recipes = new ArrayList<>();
+        for (int i = 0; i < recipesJSONArray.length(); i++) {
+            JSONObject recipeJSON = recipesJSONArray.getJSONObject(i).getJSONObject("recipe");
+            Recipe recipe = Recipe.convertJSONtoRecipe(recipeJSON);
+            recipes.add(recipe);
+        }
+        for (Recipe recipe : recipes) {
+            System.out.println(recipe.getName());
+        }
     }
 }
