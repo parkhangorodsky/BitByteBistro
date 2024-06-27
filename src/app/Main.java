37c44955b75a82af36181026c5d4854109a60392
seonghyunban <seonghyun.ban@pm.me>
2024-06-26 20:10:28 -0400

    Created initial application frame

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,15 @@
 package app;
 
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame application = new JFrame();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.setTitle("BitByteBistro");
+        application.setLocationRelativeTo(null);
+        application.setSize(800, 600);
+        application.setVisible(true);
     }
 }
