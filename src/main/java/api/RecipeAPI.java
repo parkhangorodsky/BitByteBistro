775b39bf1e8f49c74b398f2c62396af6ba1cc4a0
seonghyunban <seonghyun.ban@pm.me>
2024-06-27 02:12:29 -0400

    Created abstraction of RecipeAPI

--- src/main/java/api/RecipeAPI.java
+++ src/main/java/api/RecipeAPI.java
@@ -1,9 +1,10 @@
 package api;
 
 import entity.Recipe;
+import org.json.JSONArray;
 
 import java.util.List;
 
-public interface RecipeAPI {
-    List<Recipe> getSearchRecipeResult(String queryString);
+public interface RecipeAPI<E> {
+    JSONArray getRecipe(String queryString);
 }
