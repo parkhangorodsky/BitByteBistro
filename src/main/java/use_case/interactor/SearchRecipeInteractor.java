7f47c782df31da0fc7fda21be4107dfc5020ecfd
seonghyunban <seonghyun.ban@pm.me>
2024-06-27 02:05:20 -0400

    updated project structure configuration and dependencies

--- src/main/java/use_case/interactor/SearchRecipeInteractor.java
+++ src/main/java/use_case/interactor/SearchRecipeInteractor.java
@@ -1,13 +1,18 @@
 package use_case.interactor;
 
+import api.RecipeAPI;
 import use_case.input_data.InputBoundary;
 import use_case.input_data.SearchRecipeInputData;
 
 public class RecipeSearchInteractor implements InputBoundary {
+    private RecipeAPI recipeAPI;
 
-    public RecipeSearchInteractor() {}
+    public RecipeSearchInteractor(RecipeAPI recipeAPI) {
+        this.recipeAPI = recipeAPI;
+    }
     @Override
     public void execute(SearchRecipeInputData searchRecipeInputData) {
+        String queryString = searchRecipeInputData.getQueryString();
 
     }
 }
