a2f3466d583e36a0fc256838965694ba7ead7672
seonghyunban <seonghyun.ban@pm.me>
2024-06-27 22:23:35 -0400

    Created SearchRecipePresentor

---
+++ src/main/java/interface_adapter/presenter/SearchRecipePresenter.java
@@ -0,0 +1,33 @@
+package interface_adapter.presenter;
+
+import entity.Recipe;
+import interface_adapter.view_model.SearchRecipeViewModel;
+import interface_adapter.view_model.ViewManagerModel;
+import use_case.output_data.OutputBoundary;
+import use_case.output_data.SearchRecipeOutputData;
+import view.SearchRecipeView;
+
+import javax.swing.text.View;
+
+public class SearchRecipePresenter implements OutputBoundary {
+
+    private SearchRecipeViewModel searchRecipeViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public SearchRecipePresenter(ViewManagerModel viewManagerModel, SearchRecipeViewModel searchRecipeViewModel) {
+        this.searchRecipeViewModel = searchRecipeViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SearchRecipeOutputData recipes) {
+        searchRecipeViewModel.setRecipeSearchResult(recipes.getRecipes());
+        searchRecipeViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(searchRecipeViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+
+
+}
