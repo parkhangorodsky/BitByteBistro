2b4c0555da089d29ee3524f86b315e9219a7bbc3
seonghyunban <seonghyun.ban@pm.me>
2024-06-27 22:27:44 -0400

    Modified Config.java to adapt to the changes made

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -2,12 +2,17 @@ package app;
 
 import api.EdamamRecipeApi;
 import api.RecipeAPI;
+import interface_adapter.presenter.SearchRecipePresenter;
+import interface_adapter.view_model.SearchRecipeViewModel;
+import interface_adapter.view_model.ViewManagerModel;
 import use_case.interactor.SearchRecipeInteractor;
+import use_case.output_data.OutputBoundary;
 
 public class Config {
     private final RecipeAPI recipeAPI = new EdamamRecipeApi();
 
-    public SearchRecipeInteractor searchRecipeInteractor() {
-        return new SearchRecipeInteractor(recipeAPI);
+    public SearchRecipeInteractor searchRecipeInteractor(ViewManagerModel viewManagerModel, SearchRecipeViewModel searchRecipeViewModel) {
+        OutputBoundary searchRecipePresenter = new SearchRecipePresenter(viewManagerModel, searchRecipeViewModel);
+        return new SearchRecipeInteractor(searchRecipePresenter,recipeAPI);
     }
 }
