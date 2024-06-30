9396171708807206ea9ff46868d32780c2bdcdfc
seonghyunban <seonghyun.ban@pm.me>
2024-06-29 20:57:54 -0400

    Modified App.Main to adpat to structure change

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,8 @@
 package app;
 
+import app.gui.GUI;
 import interface_adapter.view_model.SearchRecipeViewModel;
 import interface_adapter.view_model.ViewManagerModel;
-import interface_adapter.view_model.ViewModel;
 import use_case.interactor.SearchRecipeInteractor;
 import view.SearchRecipeView;
 import view.ViewManager;
@@ -14,35 +14,13 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        // Create ViewModels
-        ViewManagerModel viewManagerModel = new ViewManagerModel();
-        SearchRecipeViewModel searchRecipeViewModel = new SearchRecipeViewModel("search recipe");
-
-        // Configuration
+        // Configuration. Config object is where we save the setting.
+        // This object contains all the initial setting value and its getter method.
         Config config = new Config();
-        SearchRecipeInteractor searchRecipeUseCase = config.searchRecipeInteractor(viewManagerModel, searchRecipeViewModel);
 
-        // Initialize Frame
-        JFrame application = new JFrame();
-        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        application.setTitle("BitByteBistro");
-        application.setLocationRelativeTo(null);
-        application.setSize(800, 600);
-
-        // Initialize Layout and Panel
-        CardLayout cardLayout = new CardLayout();
-        JPanel views = new JPanel(cardLayout);
-        application.add(views);
-
-        // Create Views
-        ViewManager viewManager = new ViewManager(views, cardLayout, viewManagerModel);
-        SearchRecipeView searchRecipeView = new SearchRecipeView(searchRecipeViewModel, searchRecipeUseCase);
-        views.add(searchRecipeView, searchRecipeView.viewname);
-
-        // Set current panel to searchRecipeView
-        viewManagerModel.setActiveView(searchRecipeView.viewname);
-        viewManagerModel.firePropertyChanged();
-        // Set application visible
-        application.setVisible(true);
+        // Get GUI from config. Currently, GUI is set to be SwingGUI in config.
+        // Then the SwingGUI is initialized.
+        GUI gui = config.getGUI();
+        gui.initialize(config);
     }
 }
