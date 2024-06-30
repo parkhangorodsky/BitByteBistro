f04df1a395e48b827f9d05a75c78f72514ad1aa6
seonghyunban <seonghyun.ban@pm.me>
2024-06-29 20:48:30 -0400

    Updated SearchRecipeInteractor to adpat to new structure

--- src/main/java/use_case/interactor/SearchRecipeInteractor.java
+++ src/main/java/use_case/interactor/SearchRecipeInteractor.java
@@ -1,6 +1,9 @@
 package use_case.interactor;
 
 import api.RecipeAPI;
+import entity.Grocery;
+import entity.Ingredient;
+import entity.Nutrition;
 import entity.Recipe;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -9,10 +12,13 @@ import use_case.input_data.SearchRecipeInputData;
 import use_case.output_data.OutputBoundary;
 import use_case.output_data.SearchRecipeOutputData;
 
+
+import java.util.Collection;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchRecipeInteractor implements InputBoundary {
+public class SearchRecipeInteractor implements InputBoundary, RecipeJSONHandler {
     private RecipeAPI recipeAPI;
     private OutputBoundary searchRecipePresenter;
 
@@ -22,18 +28,22 @@ public class SearchRecipeInteractor implements InputBoundary {
     }
     @Override
     public void execute(SearchRecipeInputData searchRecipeInputData) {
+
         String queryString = searchRecipeInputData.getQueryString();
-        JSONArray recipesJSONArray = recipeAPI.getRecipe(queryString);
+        String url = recipeAPI.createURLByRecipeName(queryString);
+        JSONArray recipesJSONArray = recipeAPI.getRecipe(url);
+
 
         List<Recipe> recipes = new ArrayList<>();
         for (int i = 0; i < recipesJSONArray.length(); i++) {
             JSONObject recipeJSON = recipesJSONArray.getJSONObject(i).getJSONObject("recipe");
-            Recipe recipe = Recipe.convertJSONtoRecipe(recipeJSON);
+            Recipe recipe =  this.convertJSONtoRecipe(recipeJSON);
             recipes.add(recipe);
         }
 
         SearchRecipeOutputData searchRecipeOutputData = new SearchRecipeOutputData(recipes);
         searchRecipePresenter.prepareSuccessView(searchRecipeOutputData);
-
     }
+
+
 }
