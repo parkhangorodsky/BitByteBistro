7bca667e081f5d6a88d13e84a5f0bd76d07564ed
seonghyunban <seonghyun.ban@pm.me>
2024-06-29 20:56:47 -0400

    Created JsonNullHandler and implemented RecipeSearchInteractor as an JsonNullHandler

---
+++ src/main/java/use_case/interactor/JsonNullHandler.java
@@ -0,0 +1,12 @@
+package use_case.interactor;
+
+import org.json.JSONObject;
+
+public interface JsonNullHandler {
+    public default String handleNullString(JSONObject json, String key) {
+        return json.isNull(key) ? "" : json.getString(key);
+    }
+    public default float handleNullFloat(JSONObject json, String key) {
+        return json.isNull(key) ? 0.0f : json.getFloat(key);
+    }
+}
