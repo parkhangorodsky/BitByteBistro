bed87d087c87d57c9300d1e7e3e4911a804b9e2f
seonghyunban <seonghyun.ban@pm.me>
2024-06-29 20:49:10 -0400

    Updated SearchRecipeController to adpat to new structure

--- src/main/java/interface_adapter/controller/SearchRecipeController.java
+++ src/main/java/interface_adapter/controller/SearchRecipeController.java
@@ -1,5 +1,6 @@
 package interface_adapter.controller;
 
+import interface_adapter.view_model.SearchRecipeViewModel;
 import use_case.input_data.InputBoundary;
 import use_case.input_data.SearchRecipeInputData;
 
