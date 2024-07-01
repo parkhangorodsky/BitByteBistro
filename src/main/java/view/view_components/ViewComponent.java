b71bcfdb48482730d7452d8da3d8a2a96e1d17c3
seonghyunban <seonghyun.ban@pm.me>
2024-06-30 23:10:03 -0400

    Created some other UI components

---
+++ src/main/java/view/view_components/ViewComponent.java
@@ -0,0 +1,17 @@
+package view.view_components;
+
+import java.awt.*;
+
+public interface ViewComponent {
+    Color patagonia = new Color(32, 46, 50);
+    Color newsStandWhite = new Color(242, 242, 243);
+
+    Color claudeBlack = new Color(60, 56, 41);
+    Color claudeWhiteEmph = new Color(207, 205, 193);
+    Color claudeWhite = new Color(238, 237, 227);
+    Color claudeOrange = new Color(217, 119, 87);
+    Color sunflower = new Color(255, 179, 44);
+
+    String defaultFont = "Hoefler Text";
+    String secondaryFont = "Avenir Next";
+}
