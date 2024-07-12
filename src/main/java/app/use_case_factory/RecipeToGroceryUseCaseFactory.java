6f2b0829e824b2b8b58550a9a4125278d9dc8db3
hoanghanhphuong <hhpfiona@gmail.com>
2024-07-12 10:29:29 -0400

    added recipe-to-grocery use case factory

--- src/main/java/app/use_case_factory/RecipeToGroceryUseCaseFactory.java
+++ src/main/java/app/use_case_factory/RecipeToGroceryUseCaseFactory.java
@@ -1,4 +1,28 @@
 package app.use_case_factory;
 
+import api.RecipeAPI;
+import interface_adapter.controller.RecipeToGroceryController;
+import interface_adapter.presenter.RecipeToGroceryPresenter;
+import interface_adapter.view_model.RecipeToGroceryViewModel;
+import interface_adapter.view_model.ViewManagerModel;
+import use_case.interactor.RecipeToGroceryInteractor;
+import view.RecipeToGroceryView;
+
 public class RecipeToGroceryUseCaseFactory {
+
+    private RecipeToGroceryUseCaseFactory() {}
+
+    public static RecipeToGroceryController create(ViewManagerModel viewManagerModel, RecipeToGroceryViewModel recipeToGroceryViewModel, RecipeAPI recipeAPI) {
+
+        // Create Presenter
+        RecipeToGroceryPresenter recipeToGroceryPresenter = new RecipeToGroceryPresenter(viewManagerModel, recipeToGroceryViewModel);
+
+        // Create UseCaseInteractor
+        RecipeToGroceryInteractor recipeToGroceryInteractor = new RecipeToGroceryInteractor(recipeToGroceryPresenter, recipeAPI);
+
+        //Create Controller
+        RecipeToGroceryController recipeToGroceryController = new RecipeToGroceryController(recipeToGroceryInteractor);
+
+        return recipeToGroceryController;
+    }
 }
