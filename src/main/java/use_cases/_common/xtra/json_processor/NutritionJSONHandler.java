63d22c11bdc55cf102174f93b4b44adbfc796982
paula arkhangorodsky <parkhangorodsky@gmail.com>
2024-07-14 20:03:58 -0400

    migrates NutritionJSONHandler

---
+++ src/main/java/use_cases/_common/xtra/json_processor/NutritionJSONHandler.java
@@ -0,0 +1,43 @@
+package use_cases._common.xtra.json_processor;
+
+import entity.Nutrition;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface NutritionJSONHandler extends JSONNullHandler {
+
+    public default List<Nutrition> convertJSONtoNutritionList(JSONObject nutritionJSON) {
+        List<Nutrition> nutritions = new ArrayList<>();
+        float calories = nutritionJSON.getFloat("calories");
+        nutritions.add(new Nutrition("calories", calories, "kcal"));
+
+        String carbsString = "CHOCDF.net";
+        String sodiumString = "NA";
+        String fatsString = "FAT";
+        List<String> nutrition_categories = new ArrayList<>();
+
+        nutrition_categories.add(carbsString);
+        nutrition_categories.add(sodiumString);
+        nutrition_categories.add(fatsString);
+
+        for (String category : nutrition_categories) {
+            float quantity = nutritionJSON.getJSONObject(category).getFloat("quantity");
+            String unit = nutritionJSON.getJSONObject(category).getString("unit");
+            nutritions.add(new Nutrition(category, quantity, unit));
+        }
+
+        return nutritions;
+    }
+
+    private List<String> JSONStringArrayToList(JSONArray jsonArray) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            list.add(jsonArray.getString(i));
+        }
+        return list;
+    }
+
+}
