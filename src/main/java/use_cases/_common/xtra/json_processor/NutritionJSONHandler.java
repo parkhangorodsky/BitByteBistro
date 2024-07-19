95670fbcdb06011925ff9a157c7ab107b53350e6
paula arkhangorodsky <parkhangorodsky@gmail.com>
2024-07-19 16:26:37 -0400

    finishes NutritionJSONHandler to deal with case when cannot retrieve nutrition info from API

--- src/main/java/use_cases/_common/xtra/json_processor/NutritionJSONHandler.java
+++ src/main/java/use_cases/_common/xtra/json_processor/NutritionJSONHandler.java
@@ -10,25 +10,33 @@ import java.util.List;
 public interface NutritionJSONHandler extends JSONNullHandler {
 
      default List<Nutrition> convertJSONtoNutritionList(JSONObject nutritionJSONObject) {
-        List<Nutrition> nutritions = new ArrayList<>();
-        float calories = nutritionJSONObject.getFloat("calories");
-        nutritions.add(new Nutrition("calories", calories, "kcal"));
+         List<Nutrition> nutritions = new ArrayList<>();
 
-        String carbs = "CHOCDF.net";
-        String sodium = "NA";
-        String fats = "FAT";
+         if (nutritionJSONObject.has("error")) {
+             nutritions.add(new Nutrition());
+         } else {
+             float calories = nutritionJSONObject.getFloat("calories");
+             nutritions.add(new Nutrition("calories", calories, "kcal"));
 
+             String carbs = "CHOCDF";
+             String sodium = "NA";
+             String fats = "FAT";
 
-        List<String> nutrition_categories = new ArrayList<>();
-        nutrition_categories.add(carbs);
-        nutrition_categories.add(sodium);
-        nutrition_categories.add(fats);
+             List<String> nutrition_categories = new ArrayList<>();
+             nutrition_categories.add(carbs);
+             nutrition_categories.add(sodium);
+             nutrition_categories.add(fats);
 
-        for (String category : nutrition_categories) {
-            float quantity = nutritionJSONObject.getJSONObject(category).getFloat("quantity");
-            String unit = nutritionJSONObject.getJSONObject(category).getString("unit");
-            nutritions.add(new Nutrition(category, quantity, unit));
-        }
+             for (String category : nutrition_categories) {
+                 JSONObject totalNutrients = nutritionJSONObject.getJSONObject("totalNutrients");
+
+                 String label = totalNutrients.getJSONObject(category).getString("label");
+                 float quantity = totalNutrients.getJSONObject(category).getFloat("quantity");
+                 String unit = totalNutrients.getJSONObject(category).getString("unit");
+
+                 nutritions.add(new Nutrition(label, quantity, unit));
+             }
+         }
 
         return nutritions;
     }
