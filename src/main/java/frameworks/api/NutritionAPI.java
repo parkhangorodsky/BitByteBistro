4d79bca6d9ea60ceef3654322c0c75fb3ebf988d
paula arkhangorodsky <parkhangorodsky@gmail.com>
2024-07-19 16:37:39 -0400

    changes NutritionAPI interface to return List<Nutrition> instead of JSONObject

--- src/main/java/frameworks/api/NutritionAPI.java
+++ src/main/java/frameworks/api/NutritionAPI.java
@@ -1,10 +1,13 @@
 package frameworks.api;
 
+import entity.Nutrition;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import use_cases.nutrition_display.use_case.input_data.NutritionDisplayInputData;
 
+import java.util.List;
+
 public interface NutritionAPI {
-    JSONObject getNutrition(NutritionDisplayInputData nutritionDisplayInputData);
+    List<Nutrition> getNutrition(NutritionDisplayInputData nutritionDisplayInputData);
 }
 
