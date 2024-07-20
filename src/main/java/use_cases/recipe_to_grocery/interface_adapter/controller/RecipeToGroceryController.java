39535645680818aff34746c7314f5d0324303ea5
Hoang Phuong Hanh <hoangph5@teach.cs.toronto.edu>
2024-07-20 12:47:42 -0400

    ongoing updates for controller

--- src/main/java/use_cases/recipe_to_grocery/interface_adapter/controller/RecipeToGroceryController.java
+++ src/main/java/use_cases/recipe_to_grocery/interface_adapter/controller/RecipeToGroceryController.java
@@ -1,18 +1,65 @@
 package use_cases.recipe_to_grocery.interface_adapter.controller;
 
+import entity.ShoppingList;
+import use_cases.recipe_to_grocery.interface_adapter.presenter.RecipeToGroceryPresenter;
 import use_cases.recipe_to_grocery.use_case.input_data.RecipeToGroceryInputBoundary;
 import use_cases.recipe_to_grocery.use_case.input_data.RecipeToGroceryInputData;
 import use_cases.recipe_to_grocery.use_case.interactor.RecipeToGroceryInteractor;
 
-public class RecipeToGroceryController {
-    final RecipeToGroceryInputBoundary recipeToGroceryInteractor;
+import entity.Recipe;
+import java.util.List;
 
-    public RecipeToGroceryController(RecipeToGroceryInputBoundary recipeToGroceryInteractor) {
+/**
+ * Controller for handling converting recipes to a grocery list of ingredients.
+ * It receives input from the view, constructs the appropriate input data, and passes it to the interactor.
+ * It encapsulates the logic for handling recipe conversion and constructing appropriate data structures.
+ */
+
+public class RecipeToGroceryController {
+
+    private final RecipeToGroceryInputBoundary recipeToGroceryInteractor;
+    private final RecipeToGroceryPresenter recipeToGroceryPresenter;
+
+    /**
+     * Class constructor for RecipeToGroceryController.
+     * @param recipeToGroceryInteractor The use case interactor to which the controller will delegate actions.
+     * @param recipeToGroceryPresenter The presenter to format output for the view.
+     */
+
+    public RecipeToGroceryController(RecipeToGroceryInputBoundary recipeToGroceryInteractor,
+                                     RecipeToGroceryPresenter recipeToGroceryPresenter) {
         this.recipeToGroceryInteractor = recipeToGroceryInteractor;
+        this.recipeToGroceryPresenter = recipeToGroceryPresenter;
     }
 
-    public void execute(String queryString) {
+    /**
+     * Fetches a list of recipes from the data source.
+     * @return List of Recipe objects fetched from the data source.
+     */
+    public List<Recipe> fetchRecipes() {
+        return recipeToGroceryInteractor.fetchRecipes();
+    }
+
+    /**
+     * Converts a list of recipes to a shopping list of ingredients.
+     * @param recipes List of Recipe objects to convert.
+     * @return List of ingredients as a shopping list.
+     */
+    public ShoppingList convertToShoppingList(List<Recipe> recipes) {
+        ConvertRecipeToShoppingListInputData inputData = new ConvertRecipeToShoppingListInputData(recipes);
+        convertRecipeToShoppingListInteractor.convertToShoppingList(inputData);
+        return convertRecipeToShoppingListPresenter.getShoppingList();
+    }
+}
+
+public void execute(String queryString) {
         RecipeToGroceryInputData recipeToGroceryInputData = new RecipeToGroceryInputData(queryString);
         recipeToGroceryInteractor.execute(recipeToGroceryInputData);
     }
 }
+
+
+
+
+
+
