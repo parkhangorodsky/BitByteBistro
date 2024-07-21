cd84a6c0a7e1ccbcbb04e6732dc7fba65cf04a1a
hhpfiona <hhpfiona@gmail.com>
2024-07-21 13:15:54 -0400

    updated input data to take user

--- src/main/java/use_cases/recipe_to_grocery/use_case/input_data/RecipeToGroceryInputData.java
+++ src/main/java/use_cases/recipe_to_grocery/use_case/input_data/RecipeToGroceryInputData.java
@@ -1,16 +1,18 @@
 package use_cases.recipe_to_grocery.use_case.input_data;
 
 import entity.User;
-import use_cases.log_in.interface_adapter.controller.LoginController;
 
 public class RecipeToGroceryInputData {
     private User user;
 
-    public RecipeToGroceryInputData(LoginController loginController) {
-        this.user = loginController.getUser();
+    public RecipeToGroceryInputData(User user) {
+        this.user = user;
     }
 
     public User getUser() {return this.user;}
 }
+
+
+
 
 
