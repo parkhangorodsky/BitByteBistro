e0fab4c0abebda114e98976abff1bbea35c533d2
hhpfiona <hhpfiona@gmail.com>
2024-07-21 22:08:43 -0400

    documentaiton for recipe to grocery presenter updated

--- src/main/java/use_cases/recipe_to_grocery/interface_adapter/presenter/RecipeToGroceryPresenter.java
+++ src/main/java/use_cases/recipe_to_grocery/interface_adapter/presenter/RecipeToGroceryPresenter.java
@@ -4,16 +4,31 @@ import use_cases._common.interface_adapter_common.view_model.models.ViewManagerM
 import use_cases.recipe_to_grocery.interface_adapter.view_model.RecipeToGroceryViewModel;
 import use_cases.recipe_to_grocery.use_case.output_data.RecipeToGroceryOutputData;
 
+/**
+ * Presenter class for handling the output of converting recipes to a grocery list.
+ * It updates the view model with the converted recipes and notifies the view manager about view changes.
+ */
 public class RecipeToGroceryPresenter implements RecipeToGroceryOutputBoundary {
 
     private RecipeToGroceryViewModel recipeToGroceryViewModel;
     private ViewManagerModel viewManagerModel;
 
+    /**
+     * Constructs a RecipeToGroceryPresenter with the specified view manager model and view model.
+     *
+     * @param viewManagerModel      The view manager model to manage view changes.
+     * @param recipeToGroceryViewModel The view model to update with grocery list results.
+     */
     public RecipeToGroceryPresenter(ViewManagerModel viewManagerModel, RecipeToGroceryViewModel recipeToGroceryViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.recipeToGroceryViewModel = recipeToGroceryViewModel;
     }
 
+    /**
+     * Prepares the success view by updating the view model with the converted recipes and notifying observers.
+     *
+     * @param shoppingLists The output data containing converted recipes as shopping lists.
+     */
     @Override
     public void prepareSuccessView(RecipeToGroceryOutputData shoppingLists) {
         recipeToGroceryViewModel.setGroceryResult(shoppingLists);
