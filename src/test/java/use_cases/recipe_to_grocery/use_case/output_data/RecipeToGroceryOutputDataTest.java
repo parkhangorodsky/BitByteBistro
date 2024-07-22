3f09253830decce29647e046682e7b344db01108
hhpfiona <hhpfiona@gmail.com>
2024-07-21 20:58:16 -0400

    updated test for output data

--- src/test/java/use_cases/recipe_to_grocery/use_case/output_data/RecipeToGroceryOutputDataTest.java
+++ src/test/java/use_cases/recipe_to_grocery/use_case/output_data/RecipeToGroceryOutputDataTest.java
@@ -2,10 +2,10 @@ package use_cases.recipe_to_grocery.use_case.output_data;
 
 import entity.Ingredient;
 import entity.Recipe;
+import entity.ShoppingList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import use_cases.search_recipe.use_case.output_data.SearchRecipeOutputData;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -14,48 +14,46 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-class SearchRecipeOutputDataTest {
+class RecipeToGroceryOutputDataTest {
 
-    SearchRecipeOutputData outputData;
-    List<Recipe> recipes;
+    RecipeToGroceryOutputData outputData;
+    List<ShoppingList> shoppingLists;
 
     @BeforeEach
     void setUp() {
-        Recipe recipe1 = Mockito.mock(Recipe.class);
-        Recipe recipe2 = Mockito.mock(Recipe.class);
+        ShoppingList shoppingList1 = Mockito.mock(ShoppingList.class);
+        ShoppingList shoppingList2 = Mockito.mock(ShoppingList.class);
 
         Ingredient ingredient1 = Mockito.mock(Ingredient.class);
         Ingredient ingredient2 = Mockito.mock(Ingredient.class);
         Ingredient ingredient3 = Mockito.mock(Ingredient.class);
         Ingredient ingredient4 = Mockito.mock(Ingredient.class);
 
-        when(recipe1.getName()).thenReturn("cake");
-        when(recipe1.getIngredientList()).thenReturn(Arrays.asList(ingredient1, ingredient2));
-        when(recipe1.getImage()).thenReturn("image1.png");
+        when(shoppingList1.getShoppingListName()).thenReturn("thursday");
+        when(shoppingList1.getListItems()).thenReturn(Arrays.asList(ingredient1, ingredient2));
 
-        when(recipe2.getName()).thenReturn("fish");
-        when(recipe2.getIngredientList()).thenReturn(Arrays.asList(ingredient3, ingredient4));
-        when(recipe2.getImage()).thenReturn("image2.png");
+        when(shoppingList1.getShoppingListName()).thenReturn("tuesday");
+        when(shoppingList1.getListItems()).thenReturn(Arrays.asList(ingredient3, ingredient4));
 
-        recipes = Arrays.asList(recipe1, recipe2);
+        shoppingLists = Arrays.asList(shoppingList1, shoppingList2);
 
-        outputData = new SearchRecipeOutputData(recipes);
+        outputData = new RecipeToGroceryOutputData(shoppingLists);
     }
 
     @Test
     void iterator() {
 
-        Iterator<Recipe> iterator = outputData.iterator();
+        Iterator<ShoppingList> iterator = outputData.iterator();
 
         assertTrue(iterator.hasNext());
-        assertEquals(iterator.next(), recipes.get(0));
+        assertEquals(iterator.next(), shoppingLists.get(0));
         assertTrue(iterator.hasNext());
-        assertEquals(iterator.next(), recipes.get(1));
+        assertEquals(iterator.next(), shoppingLists.get(1));
         assertFalse(iterator.hasNext());
     }
 
     @Test
     void getRecipes() {
-        assertEquals(outputData.getRecipes(), recipes);
+        assertEquals(outputData.getRecipes(), shoppingLists);
     }
 }
