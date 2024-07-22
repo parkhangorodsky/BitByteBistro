809b6c83f9a818604d7f45d033e23c62fe071dfb
hhpfiona <hhpfiona@gmail.com>
2024-07-21 20:57:34 -0400

    updated tests for presenter

--- src/test/java/use_cases/recipe_to_grocery/interface_adapter/presenter/RecipeToGroceryPresenterTest.java
+++ src/test/java/use_cases/recipe_to_grocery/interface_adapter/presenter/RecipeToGroceryPresenterTest.java
@@ -1,54 +1,53 @@
-package use_cases.search_recipe.interface_adapter.presenter;
+package use_cases.recipe_to_grocery.interface_adapter.presenter;
 
-import entity.Recipe;
+import entity.ShoppingList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.*;
 import use_cases._common.interface_adapter_common.view_model.models.ViewManagerModel;
-import use_cases.search_recipe.interface_adapter.view_model.SearchRecipeViewModel;
-import use_cases.search_recipe.use_case.output_data.SearchRecipeOutputData;
+import use_cases.recipe_to_grocery.interface_adapter.view_model.RecipeToGroceryViewModel;
+import use_cases.recipe_to_grocery.use_case.output_data.RecipeToGroceryOutputData;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-class SearchRecipePresenterTest {
+class RecipeToGroceryPresenterTest {
 
-    private SearchRecipeViewModel searchRecipeViewModel;
+    private RecipeToGroceryViewModel recipeToGroceryViewModel;
     private ViewManagerModel viewManagerModel;
-    private SearchRecipePresenter presenter;
+    private RecipeToGroceryPresenter presenter;
 
     @BeforeEach
     public void setUp() {
         // Create Mock View model
-        searchRecipeViewModel = mock(SearchRecipeViewModel.class);
+        recipeToGroceryViewModel = mock(RecipeToGroceryViewModel.class);
         // Create mock ViewManagerModel
         viewManagerModel = mock(ViewManagerModel.class);
-        // Create Ppresenter based on the mock view models
-        presenter = new SearchRecipePresenter(viewManagerModel, searchRecipeViewModel);
+        // Create Presenter based on the mock view models
+        presenter = new RecipeToGroceryPresenter(viewManagerModel, recipeToGroceryViewModel);
     }
 
     @Test
     public void testPrepareSuccessView() {
-        // Create a mock use case output data (recipe search result)
-        Recipe recipe1 = Mockito.mock(Recipe.class);
-        Recipe recipe2 = Mockito.mock(Recipe.class);
-        List<Recipe> recipes = Arrays.asList(recipe1, recipe2);
-        SearchRecipeOutputData outputData = new SearchRecipeOutputData(recipes);
+        // Create a mock use case output data (shopping list result)
+        ShoppingList shoppingList1 = Mockito.mock(ShoppingList.class);
+        ShoppingList shoppingList2 = Mockito.mock(ShoppingList.class);
+        List<ShoppingList> shoppingLists = Arrays.asList(shoppingList1, shoppingList2);
+        RecipeToGroceryOutputData outputData = new RecipeToGroceryOutputData(shoppingLists);
 
         // Send the mock output data to the presenter
         presenter.prepareSuccessView(outputData);
 
-        // check if setRecipeSearchResult method is called with outputData as an argument on searchRecipeViewModel
-        verify(searchRecipeViewModel).setRecipeSearchResult(outputData);
-        // check if firePropertyChanged method is called on searchRecipeViewModel
-        verify(searchRecipeViewModel).firePropertyChanged();
+        // check if setRecipeSearchResult method is called with outputData as an argument on recipeToGroceryViewModel
+        verify(recipeToGroceryViewModel).setGroceryResult(outputData);
+        // check if firePropertyChanged method is called on recipeToGroceryViewModel
+        verify(recipeToGroceryViewModel).firePropertyChanged();
         // check if setActiveView method is called with the view name of the
-        // searchRecipeViewModel as an argument on viewManagerModel
-        verify(viewManagerModel).setActiveView(searchRecipeViewModel.getViewName());
+        // recipeToGroceryViewModel as an argument on viewManagerModel
+        verify(viewManagerModel).setActiveView(recipeToGroceryViewModel.getViewName());
         // check if firePropertyChanged is called on viewManagerModel
         verify(viewManagerModel).firePropertyChanged();
     }
