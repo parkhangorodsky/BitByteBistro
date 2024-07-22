7ba6ffe4d234605f07e88f7b067dbff386647188
hhpfiona <hhpfiona@gmail.com>
2024-07-21 20:56:35 -0400

    updated test for controller

--- src/test/java/use_cases/recipe_to_grocery/interface_adapter/controller/RecipeToGroceryControllerTest.java
+++ src/test/java/use_cases/recipe_to_grocery/interface_adapter/controller/RecipeToGroceryControllerTest.java
@@ -1,75 +1,46 @@
-package use_cases.search_recipe.interface_adapter.controller;
+package use_cases.recipe_to_grocery.interface_adapter.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
-import use_cases.search_recipe.use_case.input_data.SearchRecipeInputData;
-import use_cases.search_recipe.use_case.interactor.SearchRecipeInputBoundary;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import use_cases.recipe_to_grocery.use_case.input_data.RecipeToGroceryInputData;
+import use_cases.recipe_to_grocery.use_case.interactor.RecipeToGroceryInputBoundary;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SearchRecipeControllerTest {
+class RecipeToGroceryControllerTest {
 
-    private SearchRecipeController controller;
-    private SearchRecipeInputBoundary mockInteractor;
+    private RecipeToGroceryController controller;
+    private RecipeToGroceryInputBoundary mockInteractor;
 
 
 
     @BeforeEach
     void setUp() {
-        mockInteractor = Mockito.mock(SearchRecipeInputBoundary.class);
-        controller = new SearchRecipeController(mockInteractor);
+        mockInteractor = Mockito.mock(RecipeToGroceryInputBoundary.class);
+        controller = new RecipeToGroceryController(mockInteractor);
     }
 
     @Test
-    void testBasicExecute() {
+    void testExecute() {
         String testString = "testString";
 
         // Call the method under test
-        controller.execute(testString);
+        controller.convertRecipesToGroceryList(testString);
 
-        // Create an ArgumentCaptor for SearchRecipeInputData
-        ArgumentCaptor<SearchRecipeInputData> argumentCaptor = ArgumentCaptor.forClass(SearchRecipeInputData.class);
+        // Create an ArgumentCaptor for RecipeToGroceryInputData
+        ArgumentCaptor<RecipeToGroceryInputData> argumentCaptor = ArgumentCaptor.forClass(RecipeToGroceryInputData.class);
 
         // Verify that the execute method of the interactor was called exactly once and capture the argument
         Mockito.verify(mockInteractor, Mockito.times(1)).execute(argumentCaptor.capture());
 
         // Retrieve the captured argument
-        SearchRecipeInputData capturedInputData = argumentCaptor.getValue();
+        RecipeToGroceryInputData capturedInputData = argumentCaptor.getValue();
 
         // Assert that the captured argument has the expected query string
-        assertEquals(testString, capturedInputData.getRecipeName());
-    }
-
-    @Test
-    void testAdvancedExecute() {
-
-        // Parameters
-        String testString = "testString";
-        List<String> excluded = Arrays.asList("excluded1", "excluded2");
-        List<String> diet = Arrays.asList("diet1", "diet2", "diet3");
-        List<String> health = Arrays.asList("health1", "health2");
-        List<String> cuisineType = Arrays.asList("cuisine1", "cuisine2", "cuisine3");
-        List<String> dishType = Arrays.asList("dish1");
-        List<String> mealType = new ArrayList<>();
-
-        // execute
-        controller.execute(testString, excluded, diet, health, cuisineType, dishType, mealType);
-        ArgumentCaptor<SearchRecipeInputData> argumentCaptor = ArgumentCaptor.forClass(SearchRecipeInputData.class);
-        Mockito.verify(mockInteractor, Mockito.times(1)).execute(argumentCaptor.capture());
-        SearchRecipeInputData capturedInputData = argumentCaptor.getValue();
-
-        //asserts
-        assertEquals(testString, capturedInputData.getRecipeName());
-        assertEquals(diet, capturedInputData.getDiet());
-        assertEquals(health, capturedInputData.getHealth());
-        assertEquals(cuisineType, capturedInputData.getCuisineType());
-        assertEquals(dishType, capturedInputData.getDishType());
-        assertEquals(mealType, capturedInputData.getMealType());
+        assertEquals(testString, capturedInputData.getUser());
     }
 }
 
