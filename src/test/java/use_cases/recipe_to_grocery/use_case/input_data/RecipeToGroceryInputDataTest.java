68c7a711ed8ae80b882d800d6c8e03dcf677c302
hhpfiona <hhpfiona@gmail.com>
2024-07-21 20:56:57 -0400

    updated tests for input data

--- src/test/java/use_cases/recipe_to_grocery/use_case/input_data/RecipeToGroceryInputDataTest.java
+++ src/test/java/use_cases/recipe_to_grocery/use_case/input_data/RecipeToGroceryInputDataTest.java
@@ -1,49 +1,19 @@
-package use_cases.search_recipe.use_case.input_data;
+package use_cases.recipe_to_grocery.use_case.input_data;
 
-import org.junit.jupiter.api.BeforeEach;
+import entity.User;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SearchRecipeInputDataTest {
+class RecipeToGroceryInputDataTest {
 
     @Test
     void testBasicConstructor() {
-        String recipeName = "recipe";
-        SearchRecipeInputData inputData = new SearchRecipeInputData(recipeName);
+        User user = new User("user", "email", "password", LocalDateTime.of(2023, 8, 15, 10, 30));
+        RecipeToGroceryInputData inputData = new RecipeToGroceryInputData(user);
 
-        assertFalse(inputData.isAdvanced);
-        assertEquals(recipeName, inputData.getRecipeName());
-        assertNull(inputData.getDiet());
-        assertNull(inputData.getHealth());
-        assertNull(inputData.getExcluded());
-        assertNull(inputData.getDishType());
-        assertNull(inputData.getMealType());
-        assertNull(inputData.getCuisineType());
-    }
-
-    @Test
-    void testAdvancedConstructor() {
-        String recipeName = "recipe";
-        List<String> excluded = Arrays.asList("Eggplant", "Oyster");
-        List<String> diet = Arrays.asList("Low-Fat");
-        List<String> health = Arrays.asList("Gluten-Free");
-        List<String> cuisineType = Arrays.asList("American", "Korean", "Chinese");
-        List<String> dishType = Arrays.asList("starter", "desert");
-        List<String> mealType = new ArrayList<>();
-
-        SearchRecipeInputData inputData = new SearchRecipeInputData(recipeName, excluded, diet, health, cuisineType, dishType, mealType);
-
-        assertTrue(inputData.isAdvanced);
-        assertEquals(recipeName, inputData.getRecipeName());
-        assertEquals(diet, inputData.getDiet());
-        assertEquals(health, inputData.getHealth());
-        assertEquals(dishType, inputData.getDishType());
-        assertEquals(mealType, inputData.getMealType());
-        assertEquals(cuisineType, inputData.getCuisineType());
+        assertNull(inputData.getUser());
     }
 }
