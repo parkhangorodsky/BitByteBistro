15f802f257ca36168f3cd7ce607fb93a09ad6dcf
seonghyunban <seonghyun.ban@pm.me>
2024-07-30 15:45:28 -0400

    fixed user class so that it can be auto serialized

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -25,6 +25,7 @@ public class User {
      * @param userPassword The password of the user.
      * @param createdAt The creation time of the user's account.
      */
+
     public User(String userName, String userEmail, String userPassword, LocalDateTime createdAt) {
         this.userName = userName;
         this.userEmail = userEmail; // Validate email format
@@ -34,122 +35,41 @@ public class User {
         this.recipes = new ArrayList<>();
     }
 
+    // Constructor with empty argument for MongoDB
+    public User() {}
+
     /**
      * Returns the username of the user.
      *
      * @return The username of the user.
      */
-    public String getUserName() {
-        return userName;
-    }
+    public String getUserName() {return userName;}
+    public String getUserEmail() {return userEmail;}
+    public String getUserPassword() {return userPassword;}
+    public LocalDateTime getCreatedAt() {return createdAt;}
+    public List<Recipe> getRecipes() {return recipes;}
+    public List<ShoppingList> getShoppingLists() {return shoppingLists;}
 
-    /**
-     * Returns the email address of the user.
-     *
-     * @return The email address of the user.
-     */
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    /**
-     * Returns the password of the user.
-     *
-     * @return The password of the user.
-     */
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    /**
-     * Returns the creation time of the user's account.
-     *
-     * @return The creation time of the user's account.
-     */
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    /**
-     * Returns the list of shopping lists associated with the user.
-     *
-     * @return The list of shopping lists.
-     */
-    public List<ShoppingList> getShoppingLists() {
-        return shoppingLists;
-    }
-
-    /**
-     * Returns the list of recipes associated with the user.
-     *
-     * @return The list of recipes.
-     */
-    public List<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    /**
-     * Sets the username of the user.
-     *
-     * @param userName The username to set.
-     */
-    public void setUserID(String userName) {
-        this.userName = userName;
-    }
-
-    /**
-     * Sets the email address of the user.
-     *
-     * @param userEmail The email address to set.
-     */
-    public void setEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    /**
-     * Sets the password of the user.
-     *
-     * @param userPassword The password to set.
-     */
-    public void setPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    /**
-     * Updates the email address of the user.
-     *
-     * @param newEmail The new email address.
-     */
-    public void updateEmail(String newEmail) {
-        setEmail(newEmail);
-    }
-
-    /**
-     * Updates the password of the user.
-     *
-     * @param newPassword The new password.
-     */
-    public void updatePassword(String newPassword) {
-        setPassword(newPassword);
-    }
+    public void setUserName(String userName) {this.userName = userName;}
+    public void setUserEmail(String userEmail) {this.userEmail = userEmail;}
+    public void setUserPassword(String userPassword) {this.userPassword = userPassword;}
+    public void setCreatedAt(LocalDateTime createdAt) {this.createdAt = createdAt;}
+    public void setRecipes(List<Recipe> recipes) {this.recipes = recipes;}
+    public void setShoppingLists(List<ShoppingList> shoppingLists) {this.shoppingLists = shoppingLists;}
 
     /**
      * Adds a shopping list to the user's list of shopping lists.
      *
      * @param shoppingList The shopping list to add.
      */
-    public void addShoppingList(ShoppingList shoppingList) {
-        this.shoppingLists.add(shoppingList);
-    }
+    public void addShoppingList(ShoppingList shoppingList) {this.shoppingLists.add(shoppingList);}
 
     /**
      * Adds a recipe to the user's list of recipes.
      *
      * @param recipe The recipe to add.
      */
-    public void addRecipe(Recipe recipe) {
-        this.recipes.add(recipe);
-    }
+    public void addRecipe(Recipe recipe) {this.recipes.add(recipe);}
 
     /**
      * Returns a string representation of the user.
