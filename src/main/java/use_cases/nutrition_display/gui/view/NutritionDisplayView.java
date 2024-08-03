eadb253c465be2537a1495fa368ee0a44b390c70
paula arkhangorodsky <parkhangorodsky@gmail.com>
2024-08-03 17:31:25 -0400

    deletes nutrition display use case

--- src/main/java/use_cases/nutrition_display/gui/view/NutritionDisplayView.java
+++
@@ -1,8 +0,0 @@
-package use_cases.nutrition_display.gui.view;
-
-/**
- * Overview: the View for the Nutrition Display use case, though for now this is a structural formality, as it's
- * not clear whether this use case will ever have its own View, or simply update other views when called.
- */
-public class NutritionDisplayView {
-}
