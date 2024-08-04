cf612fac4429873320e1492482f9074e486aa553
paula arkhangorodsky <parkhangorodsky@gmail.com>
2024-08-04 14:10:00 -0400

    updates InputData to have shopping list and recipe and no user

--- src/main/java/use_cases/core_functionality/CoreFunctionalityInputData.java
+++ src/main/java/use_cases/core_functionality/CoreFunctionalityInputData.java
@@ -2,6 +2,7 @@ package use_cases.core_functionality;
 
 import entity.Ingredient;
 import entity.Recipe;
+import entity.ShoppingList;
 import entity.User;
 import use_cases._common.interface_adapter_common.presenter.abstractions.PropertyChangeFirer;
 
@@ -9,27 +10,23 @@ import java.util.List;
 
 public class CoreFunctionalityInputData {
     private Recipe recipe;
-    private User loggedInUser;
-    private List<Ingredient> newIngredients;
+    private ShoppingList shoppingList;
     private PropertyChangeFirer parentModel;
 
     /**
      * Constructs an CoreFunctionalityInputData object with the specified recipe, logged-in user, and parent model.
      *
-     * @param recipe       The recipe to be added to the user's recipes.
-     * @param loggedInUser The currently logged-in user.
-     * @param newIngredients the shopping list's current list of ingredients
+     * @param recipe       The recipe to be added to the user's grocery list.
+     * @param shoppingList the shopping list the recipe will be added to
      * @param parentModel  The model that will be notified of property changes.
      */
-    public CoreFunctionalityInputData(Recipe recipe, User loggedInUser, List<Ingredient> newIngredients, PropertyChangeFirer parentModel) {
+    public CoreFunctionalityInputData(Recipe recipe, ShoppingList shoppingList, PropertyChangeFirer parentModel) {
         this.recipe = recipe;
-        this.loggedInUser = loggedInUser;
-        this.newIngredients = newIngredients;
+        this.shoppingList = shoppingList;
         this.parentModel = parentModel;
     }
 
     public Recipe getRecipe() {return recipe;}
-    public User getLoggedInUser() {return loggedInUser;}
-    public List<Ingredient> getNewIngredients() {return newIngredients;}
+    public ShoppingList getShoppingList() {return shoppingList;}
     public PropertyChangeFirer getParentModel() {return parentModel;}
 }
