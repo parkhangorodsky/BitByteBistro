544155714b733d34caa695e77d6c105e49c04411
paula arkhangorodsky <parkhangorodsky@gmail.com>
2024-08-04 13:07:58 -0400

    updates input data to take in a list of ingredients for the recipe

--- src/main/java/use_cases/core_functionality/CoreFunctionalityInputData.java
+++ src/main/java/use_cases/core_functionality/CoreFunctionalityInputData.java
@@ -2,7 +2,6 @@ package use_cases.core_functionality;
 
 import entity.Ingredient;
 import entity.Recipe;
-import entity.ShoppingList;
 import entity.User;
 import use_cases._common.interface_adapter_common.presenter.abstractions.PropertyChangeFirer;
 
@@ -11,7 +10,7 @@ import java.util.List;
 public class CoreFunctionalityInputData {
     private Recipe recipe;
     private User loggedInUser;
-    private List<Ingredient> shoppingListIngredients;
+    private List<Ingredient> newIngredients;
     private PropertyChangeFirer parentModel;
 
     /**
@@ -19,18 +18,18 @@ public class CoreFunctionalityInputData {
      *
      * @param recipe       The recipe to be added to the user's recipes.
      * @param loggedInUser The currently logged-in user.
-     * @param shoppingListIngredients the shopping list's current list of ingredients
+     * @param newIngredients the shopping list's current list of ingredients
      * @param parentModel  The model that will be notified of property changes.
      */
-    public CoreFunctionalityInputData(Recipe recipe, User loggedInUser, List<Ingredient> shoppingListIngredients, PropertyChangeFirer parentModel) {
+    public CoreFunctionalityInputData(Recipe recipe, User loggedInUser, List<Ingredient> newIngredients, PropertyChangeFirer parentModel) {
         this.recipe = recipe;
         this.loggedInUser = loggedInUser;
-        this.shoppingListIngredients = shoppingListIngredients;
+        this.newIngredients = newIngredients;
         this.parentModel = parentModel;
     }
 
     public Recipe getRecipe() {return recipe;}
     public User getLoggedInUser() {return loggedInUser;}
-    public List<Ingredient> getShoppingListIngredients() {return shoppingListIngredients;}
+    public List<Ingredient> getNewIngredients() {return newIngredients;}
     public PropertyChangeFirer getParentModel() {return parentModel;}
 }
