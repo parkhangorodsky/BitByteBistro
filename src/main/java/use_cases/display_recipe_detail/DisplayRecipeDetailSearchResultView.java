014fcaf7123b9fb9eee1f8fc2f87a10053d830a3
RqedK <raedkabir1@gmail.com>
2024-08-06 14:54:40 -0400

    Fixed code convention errors

--- src/main/java/use_cases/display_recipe_detail/DisplayRecipeDetailSearchResultView.java
+++ src/main/java/use_cases/display_recipe_detail/DisplayRecipeDetailSearchResultView.java
@@ -1,9 +1,7 @@
 package use_cases.display_recipe_detail;
 
-import app.local.LoggedUserData;
 import entity.Recipe;
-import entity.ShoppingList;
-import entity.User;
+
 import use_cases._common.gui_common.abstractions.NightModeObject;
 import use_cases._common.gui_common.view_components.round_component.RoundButton;
 import use_cases.add_to_my_recipe.AddToMyRecipeController;
@@ -11,13 +9,11 @@ import use_cases.core_functionality.CoreFunctionalityController;
 import use_cases.add_new_grocery_list.AddNewGroceryListController;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
 import java.beans.PropertyChangeEvent;
-import java.util.List;
+
 
 public class DisplayRecipeDetailSearchResultView extends DisplayRecipeDetailView implements NightModeObject {
-    private AddToMyRecipeController addToMyRecipeController;
+    private final AddToMyRecipeController addToMyRecipeController;
     RoundButton addToRecipesButton;
 
 
@@ -44,9 +40,7 @@ public class DisplayRecipeDetailSearchResultView extends DisplayRecipeDetailView
         addToRecipesButton = new RoundButton("Add To My Recipes");
         Recipe recipe = viewModel.getRecipe();
 
-        addToRecipesButton.addActionListener(e -> {
-            addToMyRecipeController.execute(recipe, viewModel);
-        });
+        addToRecipesButton.addActionListener(_ -> addToMyRecipeController.execute(recipe, viewModel));
 
        buttonPanel.remove(closeButton);
        buttonPanel.revalidate();
@@ -62,13 +56,13 @@ public class DisplayRecipeDetailSearchResultView extends DisplayRecipeDetailView
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName().equals("recipe already exists")) {
-            JOptionPane.showMessageDialog((JFrame) SwingUtilities.getWindowAncestor(this),
+            JOptionPane.showMessageDialog(SwingUtilities.getWindowAncestor(this),
                     "Recipe already exists.",
                     "",
                     JOptionPane.ERROR_MESSAGE);
         } else if (evt.getPropertyName().equals("added recipe")) {
-            JOptionPane.showMessageDialog((JFrame) SwingUtilities.getWindowAncestor(this),
-                    "Succesfully added.", "",
+            JOptionPane.showMessageDialog(SwingUtilities.getWindowAncestor(this),
+                    "Successfully added.", "",
                     JOptionPane.INFORMATION_MESSAGE);
         } else if (evt.getPropertyName().equals("nightMode")) {
             toggleNightMode();
