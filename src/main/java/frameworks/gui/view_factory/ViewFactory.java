d90711444a41bf32725b220b2902fe46382cc6a2
seonghyunban <seonghyun.ban@pm.me>
2024-08-07 22:52:54 -0400

    design pattern update

--- src/main/java/frameworks/gui/view_factory/ViewFactory.java
+++ src/main/java/frameworks/gui/view_factory/ViewFactory.java
@@ -2,31 +2,26 @@ package frameworks.gui.view_factory;
 
 import app.config.Config;
 import frameworks.gui.view_factory.division.ViewFactoryDivision;
-import use_cases._common.gui_common.view.HomeView;
-import use_cases.add_to_my_recipe.MyRecipeView;
-import use_cases.core_functionality.MyGroceryView;
-import use_cases.log_in.gui.view.LoginView;
-import use_cases.search_recipe.gui.view.SearchRecipeView;
-import use_cases.sign_up.gui.view.SignUpView;
+import use_cases._common.gui_common.abstractions.View;
 
-public abstract class ViewFactory {
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class ViewFactory{
+
     protected Config config;
-    protected ViewFactoryDivision<SearchRecipeView> searchRecipeViewDivision;
-    protected ViewFactoryDivision<LoginView> loginViewDivision;
-    protected ViewFactoryDivision<SignUpView> signUpViewDivision;
-    protected ViewFactoryDivision<HomeView> homeViewDivision;
-    protected ViewFactoryDivision<MyRecipeView> myRecipeViewDivision;
-    protected ViewFactoryDivision<MyGroceryView> myGroceryViewDivision;
+    protected Map<String, ViewFactoryDivision> divisions = new HashMap<>();
 
     public ViewFactory(Config config) {
         this.config = config;
     }
 
-    public abstract SearchRecipeView generateSearchRecipeView();
-    public abstract LoginView generateLoginView();
-    public abstract SignUpView generateSignUpView();
-    public abstract HomeView generateHomeView();
-    public abstract MyRecipeView generateMyRecipeView();
-    public abstract MyGroceryView generateMyGroceryView();
+    protected void addDivision(String name, ViewFactoryDivision division) {
+        divisions.put(name, division);
+    }
+
+    public View generate(String viewName) {
+        return divisions.get(viewName).generate(config);
+    }
 
 }
--- src/main/java/frameworks/gui/view_factory/division/HomeViewSwingDivision.java
+++ src/main/java/frameworks/gui/view_factory/division/HomeViewSwingDivision.java
@@ -1,12 +1,14 @@
 package frameworks.gui.view_factory.division;
 
 import app.config.Config;
+import use_cases._common.gui_common.abstractions.View;
 import use_cases._common.gui_common.view.HomeView;
 
-public class HomeViewSwingDivision implements ViewFactoryDivision<HomeView> {
+public class HomeViewSwingDivision implements ViewFactoryDivision {
     @Override
-    public HomeView generate(Config config) {
+    public View generate(Config config) {
         return new HomeView(
                 config.getViewManagerModel());
+
     }
 }
