e0337adb482e8137c500e4c7fa0fe917afcf063f
Hoang Hanh Phuong <hhpfiona@gmail.com>
2024-08-14 00:44:07 -0400

    handles when user enters empty name or cancel adding new grocery list

--- src/main/java/use_cases/display_recipe_detail/DisplayRecipeDetailSearchResultView.java
+++ src/main/java/use_cases/display_recipe_detail/DisplayRecipeDetailSearchResultView.java
@@ -104,9 +104,14 @@ public class DisplayRecipeDetailSearchResultView extends DisplayRecipeDetailView
 
     private void createNewGroceryListAndAdd(Recipe recipe) {
         String newListName = JOptionPane.showInputDialog((JFrame) SwingUtilities.getWindowAncestor(this), "Enter name for new grocery list:");
-        //need to handle when this is empty
-        //need to handle when cancel is pressed
-        if (newListName != null && !newListName.trim().isEmpty()) {
+        if (newListName == null) {
+            return;
+        } else if (newListName.trim().isEmpty()) {
+            JOptionPane.showMessageDialog(SwingUtilities.getWindowAncestor(this),
+                    "Name cannot be empty.",
+                    "",
+                    JOptionPane.ERROR_MESSAGE);
+        } else {
             addNewGroceryListController.execute(newListName, viewModel);
         }
         ShoppingList newShoppingList = user.getShoppingList(newListName);
