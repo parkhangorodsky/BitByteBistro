87efc1259e1d85ae099d0a5f9cb672e27b356d0d
Hoang Hanh Phuong <hhpfiona@gmail.com>
2024-08-20 20:28:05 -0400

    documentation for core functionality output oundary

--- src/main/java/use_cases/core_functionality/CoreFunctionalityOutputBoundary.java
+++ src/main/java/use_cases/core_functionality/CoreFunctionalityOutputBoundary.java
@@ -1,5 +1,18 @@
 package use_cases.core_functionality;
 
+/**
+ * The `CoreFunctionalityOutputBoundary` interface defines the contract for preparing the success view
+ * in response to core functionality operations. It is used to present the results of use cases related
+ * to managing recipes and shopping lists.
+ */
 public interface CoreFunctionalityOutputBoundary {
+
+    /**
+     * Prepares the success view with the provided output data. This method is called to present the
+     * results of a successful operation, such as adding a recipe to a shopping list.
+     *
+     * @param outputData The data to be used for preparing the success view. This includes the updated
+     *                   shopping list and any necessary information for the view model.
+     */
     void prepareSuccessView(CoreFunctionalityOutputData outputData);
 }
